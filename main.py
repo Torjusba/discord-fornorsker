25456d008513185e51d268fcd6083436540a14ba
Torjus <torjus.bakkene@gmail.com>
2018-09-26 16:21:29 +0200

    Pyntet på kode. Lagt i klasser og funksjoner

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
-from parsehtml import HTMLTableParser
+#! /usr/bin/python3
+
+# Nettsiden:
+# https://www.sprakradet.no/sprakhjelp/Skriverad/Avloeysarord/
+from pdparser import PDParser
+
+
+def recognize(_importlist, _avloeyserlist):
+    inp = input("Ord >")
+    if inp in _importlist:
+        print("Gjenkjent")
+        index = _importlist.index(inp)
+        print("Index: {}".format(index))
+        alternativ = _avloeyserlist[index]
+        print(alternativ)
+
 
 def main():
-    hp = HTMLTableParser()
-    table = hp.parse_url(url)[0][1] # Grabbing the table from the tuple
-    table.head()
+    pdparser = PDParser('https://www.sprakradet.no/sprakhjelp/Skriverad/Avloeysarord/')
+    while True:
+        recognize(pdparser.importlist, pdparser.avloeyserlist)
 
 if __name__=="__main__":
     main()
--- pdparser.py
+++ pdparser.py
@@ -1,27 +1,14 @@
-# Nettsiden:
-# https://www.sprakradet.no/sprakhjelp/Skriverad/Avloeysarord/
-
-
 import pandas as pd
 import numpy as np
 
 
+class PDParser:
+    def __init__(self, _url):
+        df = pd.read_html(_url,header=0)[0]
+        ddf = df.to_dict()
 
-df = pd.read_html('http://www.sprakradet.no/sprakhjelp/Skriverad/Avloeysarord/',header=0)[0]
+        self.importlist = list(ddf['Importord'].values())
+        self.avloeyserlist = list(ddf['AvlÃ¸serord'].values())
 
-ddf = df.to_dict()
-#ndf = df.values
-
-importlist = list(ddf['Importord'].values())
-avloeyserlist = list(ddf['AvlÃ¸serord'].values())
-
-print(importlist)
-
-while True:
-    inp = input("Ord >")
-    if inp in importlist:
-        print("Gjenkjent")
-        index = importlist.index(inp)
-        print("Index: {}".format(index))
-        alternativ = avloeyserlist[index]
-        print(alternativ)
+    
+    
