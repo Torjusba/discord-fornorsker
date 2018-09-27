2f78ad89c61dbe95bb2947115536b34e60000a3c
Torjus <torjus.bakkene@gmail.com>
2018-09-27 16:09:54 +0200

    Mer behandling av importord før gjenkjenning. Fjerning av parentes o.l..

--- pdparser.py
+++ pdparser.py
@@ -9,8 +9,10 @@ class PDParser:
         ae = "Ã¦" #æ
         oe = "Ã¸" #ø
         aa="Ã¥" #å
-
         return str(_word).replace(ae,"æ").replace(oe,'ø').replace(aa,'å').lower()
+        
+    def remove_parenthesis(self, _word):
+        return _word.split('(')[0]
 
     def __init__(self, _url):
         df = pd.read_html(_url,header=0)[0]
@@ -25,11 +27,15 @@ class PDParser:
 
         #Remove label entries
         for _w in self.importlist:
+            _noparstr = self.remove_parenthesis(str(_w))
+
             w=str(_w) #Fikser problemer med at æøå tolkes som float
             if len(w)==1:
                 self.avloeyserlist.pop(self.importlist.index(w))
                 self.importlist.remove(w)
-                
+            else:
+                self.importlist[self.importlist.index(_w)] = _noparstr[:-1]
+
 
     
     
