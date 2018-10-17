c1b6305d7f6ee592c7ba300122873b76ea811df8
Torjusba <torjus.bakkene@gmail.com>
2018-10-17 17:41:02 +0000

    Henter token fra en separat fil

--- discordbot.py
+++ discordbot.py
@@ -5,6 +5,8 @@ import asyncio
 import time
 import random
 
+from privatetoken import getToken
+
 from pdparser import PDParser
 
 def recognize(_word, _importlist, _avloeyserlist):
@@ -17,7 +19,7 @@ pdparser = PDParser('https://www.sprakradet.no/sprakhjelp/Skriverad/Avloeysarord
 Client = discord.Client()
 client = commands.Bot(command_prefix="#")
 
-privatetoken = "NDk0NTQ3NzcxNjI2MzU2NzU4.Dp5-ZQ.KWnfuA7fgEfX48eeWBlIqcxEhY4"
+privatetoken = getToken()
 
 def check_hit(_hitrate):
 	roll = random.randint(0,100)
@@ -40,13 +42,18 @@ async def on_message(message):
     _message = message.content.lower()
     
     if _message == "@språkrådet hjelp":
-        await client.send_message(message.channel, "Svarer på 50% av importord. https://www.sprakradet.no/sprakhjelp/Skriverad/Avloeysarord/'")
+        await client.send_message(message.channel, "Svarer på 20% av importord. https://www.sprakradet.no/sprakhjelp/Skriverad/Avloeysarord/'")
     
-    if not check_hit(50):
+    if not check_hit(20):
         return()
 
     _arr = _message.split(" ")    
 
+#    for w in _arr:
+#        if (w=="yeet"):
+#            _message = "Du brukte ordet 'yeet'. Dette er et lånord, og bør erstattes med å bare dø"
+#            await client.send_message(message.channel, _message)
+    
     for w in _arr:
         alt = recognize(w, pdparser.importlist, pdparser.avloeyserlist)
         if len(alt)>0:
