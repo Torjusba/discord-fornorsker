05f3aac2c4b83840f8e4762bf4919cfc097027b2
Torjus Bakkene <torjus.bakkene@gmail.com>
2021-06-10 00:45:22 +0200

    Regex for smartere retting :)

--- pkg/analysis/analysis.go
+++ pkg/analysis/analysis.go
@@ -1,11 +1,26 @@
 package analysis
 
-import "strings"
+import (
+	"fmt"
+	"regexp"
+	"strings"
+)
+
+// Match 1 whitespace or common punctuation
+const WORD_SEPARATOR_REGEX string = "[\\s,\\./()\\?\\\\\\*\\+\\-~#&\\^%]+"
 
 func doesSentenceContainWord(sentence, word string) bool {
-	lowerCaseSentence := strings.ToLower(sentence)
+	// Pad sentence for regex match on edges
+	lowerCaseSentence := " " + strings.ToLower(sentence) + " "
 	lowerCaseWord := strings.ToLower(word)
-	return strings.Contains(lowerCaseSentence, lowerCaseWord)
+
+	// Complex check for words in a context
+	wordRegex := WORD_SEPARATOR_REGEX + lowerCaseWord + WORD_SEPARATOR_REGEX
+	didRegexMatch, err := regexp.Match(wordRegex, []byte(lowerCaseSentence))
+	if err != nil {
+		panic(fmt.Sprintf("Error matching regex to sentence: %s", err))
+	}
+	return didRegexMatch
 }
 
 func WhichWordsAreInSentence(sentence string, words []string) []string {
